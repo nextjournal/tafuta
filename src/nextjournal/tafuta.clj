e6b3ee635804d232c2581295d780f84da19ffc2f
FiV0 <finn.volkel@gmail.com>
2021-04-07 14:08:20 +0100

    Add search-file and search-directory

--- src/nextjournal/tafuta.clj
+++ src/nextjournal/tafuta.clj
@@ -1,6 +1,7 @@
 (ns nextjournal.tafuta
   "A namespace for leveraging unix text search tools like \"ag\" or \"ack\"."
   (:require [babashka.process :as process]
+            [clojure.java.io :as io]
             [clojure.string :as str]
             [jsonista.core :as json]))
 
@@ -65,8 +66,7 @@
        (map #(json/read-value % json/keyword-keys-object-mapper))))
 
 (defn parse-rg-jsonlines
-  "Parses jsonlines as returned by `rg` and massages them into the clojure format
-  returned by tafuta."
+  "Parses jsonlines as returned by `rg` and massages them into the clojure data."
   [s]
   (let [jsonlines (read-jsonlines s)]
     (->> (filter #(= (:type %) "match") jsonlines)
@@ -77,21 +77,157 @@
                  :file (-> match :path :text)
                  :line (-> match :lines :text (str/replace "\n" ""))})))))
 
+(defn remove-duplicate
+  "Returns a lazy sequence of the elements of coll with duplicates removed using a predicate"
+  [coll pred]
+  (let [step (fn step [xs seen]
+               (lazy-seq
+                ((fn [[f :as xs] seen]
+                   (when-let [s (seq xs)]
+                     (if (contains? seen (pred f) )
+                       (recur (rest s) seen)
+                       (cons f (step (rest s) (conj seen (pred f)))))))
+                 xs seen)))]
+    (step coll #{})))
+
 (defn search
   "Searches for `pattern` in `dir`. Uses the best available searcher installed.
-  Returns nil if no results are found. Raises an error for an exit code different to 0 or 1."
-  [pattern dir]
-  (let [dir (if (instance? java.io.File dir) (.getPath dir) dir)]
-    (if-let [searcher (find-searcher)]
-      (let [{:keys [exit out err]} @(process/process (concat [searcher pattern dir] (extra-arguments searcher)))]
-        (case exit
-          0 (let [out (slurp out)]
-              (case searcher
-                "ag" (->> (str/split out #"\n\n")
-                          (mapcat parse-ackmate))
-                "rg" (parse-rg-jsonlines out)))
-          1 nil
-          (throw (ex-info (str searcher " exited with error code " exit) {:out out :err err}))))
-      (throw (ex-info (str "No supported code search tool found!\n"
-                           "Please make sure you have one of the supported search tools installed.")
-                      {})))))
+  Returns nil if no results are found. Raises an error for an exit code different to 0 or 1.
+  The default directory is the path from which the JVM was invoked.
+  An optional options map can be passed as third argument. Current options
+  include:
+   - `duplicates?`"
+  ([pattern] (search pattern "."))
+  ([pattern dir]
+   (let [dir (if (instance? java.io.File dir) (.getPath dir) dir)]
+     (if-let [searcher (find-searcher)]
+       (let [{:keys [exit out err]} @(process/process (concat [searcher pattern dir] (extra-arguments searcher)))]
+         (case exit
+           0 (let [out (slurp out)]
+               (case searcher
+                 "ag" (->> (str/split out #"\n\n")
+                           (mapcat parse-ackmate))
+                 "rg" (parse-rg-jsonlines out)))
+           1 nil
+           (throw (ex-info (str searcher " exited with error code " exit) {:out out :err err}))))
+       (throw (ex-info (str "No supported code search tool found!\n"
+                            "Please make sure you have one of the supported search tools installed.")
+                       {})))))
+  ([pattern dir {:keys [duplicates?]}]
+   (cond-> (search pattern dir)
+     (not duplicates?)
+     (remove-duplicate :file))))
+
+(defn try-convert-to-regex
+  "Tries to convert a string to a regex, returns the string if not a valid
+  Java regex."
+  [s]
+  (try
+    (re-pattern s)
+    (catch java.util.regex.PatternSyntaxException _e
+      s)))
+
+(defn git-dir?
+  "Returns true if the given directory is the root of a git directory."
+  [dir]
+  (let [git-dir (io/file dir ".git")]
+    (and (.exists git-dir) (.isDirectory git-dir))))
+
+(comment
+  (git-dir? (io/file "."))
+  (git-dir? (io/file "/"))
+  )
+
+(defn git-files
+  "Return all files currently under source control of given git root directory."
+  [dir]
+  {:pre [(git-dir? dir)]}
+  (let [{:keys [exit out err]} @(process/process ["git" "ls-files"] {:dir dir})]
+    (case exit
+      0 (->> (slurp out)
+             (str/split-lines)
+             (map #(io/file %)))
+      (throw (ex-info (str "git ls-files exited with error code " exit) {:out out :err err})))))
+
+(comment
+  (git-files (io/file "."))
+  (git-files (io/file "/"))
+  )
+
+(def non-escaped-stars-regex #"(?<!\\)\*")
+
+(defn replace-non-escaped-stars
+  "Replaces non escaped stars with \".*\"."
+  [pat]
+  (str/replace pat non-escaped-stars-regex ".*"))
+
+(comment
+  (replace-non-escaped-stars "\\*123")
+  (replace-non-escaped-stars "\\*123*")
+  )
+
+(defn search-file
+  "Recursively searches for files matching pattern in a given directory.
+  The default directory is the path from which the JVM was invoked.
+  Returns the relative paths with respect to the given directory as data.
+  A non escaped star is interpreted as a wildcard character."
+  ([pattern] (search-file pattern (io/file ".")))
+  ([pattern dir]
+   (let [files (git-files dir)
+         regex (-> pattern replace-non-escaped-stars try-convert-to-regex)
+         results (cond
+                   (empty? pattern)
+                   files
+
+                   (= (type regex) java.lang.String)
+                   (filter #(str/includes? (.getName %) pattern) files)
+
+                   :else
+                   (filter #(re-matches regex (.getName %)) files))]
+     (map (fn [file] {:path (str file)}) results))))
+
+(comment
+  (search-file "*clj")
+  (search-file "taf*.clj")
+  )
+
+(defn git-dirs
+  "Return all directories currently under source control of given git root directory."
+  [dir]
+  {:pre [(git-dir? dir)]}
+  (let [{:keys [exit out err]} @(process/process ["git" "ls-tree" "-d" "-r" "HEAD" "--name-only"] {:dir dir})]
+    (case exit
+      0 (->> (slurp out)
+             (str/split-lines)
+             (map #(io/file %)))
+      (throw (ex-info (str "git ls-files exited with error code " exit) {:out out :err err})))))
+
+(comment
+  (git-dirs (io/file "."))
+  (git-dirs (io/file "/"))
+  )
+
+(defn search-directory
+  "Recursively searches for directories matching pattern in a given directory.
+  The default directory is the path from which the JVM was invoked.
+  Returns the relative paths with respect to the given directory as data.
+  A non escaped star is interpreted as a wildcard character."
+  ([pattern] (search-directory pattern (io/file ".")))
+  ([pattern dir]
+   (let [dirs (git-dirs dir)
+         regex (-> pattern replace-non-escaped-stars try-convert-to-regex)
+         results (cond
+                   (empty? pattern)
+                   dirs
+
+                   (= (type regex) java.lang.String)
+                   (filter #(str/includes? (.getName %) pattern) dirs)
+
+                   :else
+                   (filter #(re-matches regex (.getName %)) dirs))]
+     (map (fn [file] {:path (str file)}) results))))
+
+(comment
+  (search-directory "nextjournal")
+  (search-directory "*editor*")
+  )
--- test/nextjournal/tafuta_tests.clj
+++ test/nextjournal/tafuta_tests.clj
@@ -37,20 +37,20 @@
     (is (= nil
            (tafuta/search "adsfadsfasdfasdfafds" "src/"))
         "empty results not working properly")
-    (is (= '({:line-number 26,
+    (is (= '({:line-number 27,
               :occurences [[6 10]],
               :line "(defn parse-line",
               :file "src/nextjournal/tafuta.clj"}
-             {:line-number 58,
+             {:line-number 59,
               :occurences [[10 10]],
               :line "     (map parse-line lines)",
               :file "src/nextjournal/tafuta.clj"})
            (tafuta/search "parse-line" "src/")))
-    (is (= '({:line-number 26,
+    (is (= '({:line-number 27,
               :occurences [[6 10]],
               :line "(defn parse-line",
               :file "src/nextjournal/tafuta.clj"}
-             {:line-number 58,
+             {:line-number 59,
               :occurences [[10 10]],
               :line "     (map parse-line lines)",
               :file "src/nextjournal/tafuta.clj"})
@@ -62,20 +62,20 @@
       (is (= nil
              (tafuta/search "adsfadsfasdfasdfafds" "src/"))
           "empty results not working properly")
-      (is (= '({:line-number 26,
+      (is (= '({:line-number 27,
                 :occurences [[6 10]],
                 :line "(defn parse-line",
                 :file "src/nextjournal/tafuta.clj"}
-               {:line-number 58,
+               {:line-number 59,
                 :occurences [[10 10]],
                 :line "     (map parse-line lines)",
                 :file "src/nextjournal/tafuta.clj"})
              (tafuta/search "parse-line" "src/")))
-      (is (= '({:line-number 26,
+      (is (= '({:line-number 27,
                 :occurences [[6 10]],
                 :line "(defn parse-line",
                 :file "src/nextjournal/tafuta.clj"}
-               {:line-number 58,
+               {:line-number 59,
                 :occurences [[10 10]],
                 :line "     (map parse-line lines)",
                 :file "src/nextjournal/tafuta.clj"})
@@ -87,3 +87,48 @@
       (is (thrown? clojure.lang.ExceptionInfo
                    (tafuta/search "adsfadsfasdfasdfafds" "src/")))
       "no searcher not throwing")))
+
+
+(deftest search-different-args-test
+  (testing "nextjournal.tafuta/search for different args"
+    (is (= '({:line-number 1,
+              :occurences [[16 6]],
+              :line "(ns nextjournal.tafuta",
+              :file "src/nextjournal/tafuta.clj"})
+           (tafuta/search "tafuta" (io/file "src/nextjournal")))
+        "search with dir as File not working")
+    (is (= '({:line-number 1,
+              :occurences [[16 6]],
+              :line "(ns nextjournal.tafuta",
+              :file "src/nextjournal/tafuta.clj"})
+           (tafuta/search "tafuta" "src/nextjournal"))
+        "search with dir as String not working")
+    (is (= '({:line-number 80,
+              :occurences [[6 16]],
+              :line "(defn remove-duplicate",
+              :file "src/nextjournal/tafuta.clj"}
+             {:line-number 119,
+              :occurences [[6 16]],
+              :line "     (remove-duplicate :file))))",
+              :file "src/nextjournal/tafuta.clj"})
+           (tafuta/search "remove-duplicate" "src" {:duplicates? true}))
+        "search-pattern with duplicates not working")))
+
+(deftest search-file-test
+  (testing "search-file"
+    (is (= '({:path "src/nextjournal/tafuta.clj"})
+           (tafuta/search-file "tafuta.clj"))
+        "search-file in current directory not working")
+    (is (= '({:path "src/nextjournal/tafuta.clj"}
+             {:path "test/nextjournal/tafuta_tests.clj"})
+           (tafuta/search-file "tafuta*clj"))
+        "search-file with wildcard not working")))
+
+(deftest search-directory-test
+  (testing "search-directory"
+    (is (= '({:path "src/nextjournal"} {:path "test/nextjournal"})
+           (tafuta/search-directory "nextjournal"))
+        "search-directory in current directory not working")
+    (is (= '({:path "src/nextjournal"} {:path "test/nextjournal"})
+           (tafuta/search-directory "*journal"))
+        "search-with wildcard not working")))
